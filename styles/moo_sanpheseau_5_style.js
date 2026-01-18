c47b3914f73f6d30c5b4c2da88325cf50865abdc
k-worapoth <k.worapoth@gmail.com>
2026-01-18 15:47:54 +0700

    Add files via upload

--- styles/moo_sanpheseau_5_style.js
+++ styles/moo_sanpheseau_5_style.js
@@ -21,7 +21,7 @@ var style_moo_sanpheseau_5 = function(feature, resolution){
         labelText = String(feature.get("moo"));
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(231,113,72,0.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(231,113,72,0.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
